882035ce39bd15da103d70999a60983a37fef740
rheaajep <48777082+rheaajep@users.noreply.github.com>
2019-04-25 15:04:54 -0400

    Update mad-lib story.py

--- Small Projects/mad-lib story.py
+++ Small Projects/mad-lib story.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 print("Welcome! to mad libs generator\nlet's build a story")
 option='no'
 while option=='yes'or'Yes'or'YES':
@@ -19,20 +13,3 @@ while option=='yes'or'Yes'or'YES':
     print(noun1,"struck a meteor, unaware that it would lead to",noun3,".\n",noun2,"kept following it, in the hope of ",verb1,noun1,"but everything was",adjective,".\nSoon,",noun2,"realized that they could",verb2,"the inhabitants of planet",noun4,",to",verb3,noun1,"back.")
     option=input("do you wish to make your story again: ")
     print(option)
-
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
